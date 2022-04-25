da45021087fa9e4b8c20c753f471f5e3e9c4b01f
dustin-oz <schelldd@gmail.com>
2022-04-25 08:40:21 -0500

    Completed last ruby challenege question

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -43,13 +43,53 @@ p additionBot nums_array2
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
-# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
-
-
-
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+
+class Bike
+    def initialize(model, current_speed=0)
+        @model = model
+        @wheels = 2
+        @current_speed = current_speed
+        
+        def bike_info
+            "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph"
+        end
+
+        def pedal_faster speed_increase
+            @current_speed += speed_increase #add given speed_increase to current speed
+        end
+
+        def brake speed_decrease
+            @current_speed = @current_speed - speed_decrease # remove given speed_increase to current speed
+            if @current_speed.positive?
+                @current_speed 
+            elsif 
+                @current_speed.negative? # trap for when # goes negatove
+                @current_speed = 0
+            end
+        end
+    end
+end
+
+my_bike = Bike.new 'Trek'
+p my_bike.bike_info
+# 
+# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+# My Output: "The Trek bike has 2 wheels and is going 0 mph"
+
+my_bike = Bike.new 'Trek'
+p my_bike.pedal_faster(10)
+p my_bike.pedal_faster(18)
+p my_bike.brake(5)
+p my_bike.brake(25)
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+# MY OUTPUT:
+# 10
+# 28
+# 23
+# 0
